f9423c52042e259fd8e8388bea14d970f3aecdba
cynthiatakematu <149814289+cynthiatakematu@users.noreply.github.com>
2025-05-06 22:15:45 -0300

    Versao com o arquivo novamedia.py na funcA

---
+++ novamedia.py
@@ -0,0 +1,10 @@
+#meu primeiro teste com o git
+titulo = "Media de dois valores"
+print(f'{titulo:^30}')
+
+x = float(input('Entre com o valor de x: '))
+y = float(input('Entre com o valor de y: '))
+media = (x+y)/2
+print("O valor de x é", x, "!")
+print('O valor de y é', y)
+print('A media dos valores é', round(media, 3))
